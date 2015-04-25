5e2bd42f268133096f3475a9654a010d246212fc
Joshua K. Farrar <joshua@sent1nel.me>
2015-04-25 19:25:27 -0400

    test for server message

--- spec/server_spec.rb
+++ spec/server_spec.rb
@@ -1,11 +1,16 @@
 require "spec_helper"
+require "net/telnet"
 
 require "server.rb"
 
 describe VippyMUD::Server do
   describe '#start' do
     it 'should start a server' do
-      true.should be_false
+      buffer = ''
+      localhost = Net::Telnet::new("Host" => "localhost",
+                                   "Port" => 8080)
+      localhost.cmd("list") { |c| buffer << c }
+      expect(buffer).to match("hej! :) welcome to \e[5;94;47mVippyMUD\e[0m! :3!\n")
     end
   end
 end
