4be4d861d4b3b79fcaf72369af3d02886426bc30
Joshua K. Farrar <joshua@sent1nel.me>
2015-04-25 19:29:43 -0400

    add local server to tests

--- spec/server_spec.rb
+++ spec/server_spec.rb
@@ -4,13 +4,24 @@ require "net/telnet"
 require "server.rb"
 
 describe VippyMUD::Server do
+  before(:each) do
+    @server = VippyMUD::Server.new(8081)
+    @server.start
+  end
+
   describe '#start' do
     it 'should start a server' do
       buffer = ''
+
       localhost = Net::Telnet::new("Host" => "localhost",
-                                   "Port" => 8080)
+                                   "Port" => 8081)
       localhost.cmd("list") { |c| buffer << c }
+
       expect(buffer).to match("hej! :) welcome to \e[5;94;47mVippyMUD\e[0m! :3!\n")
     end
+  end
+
+  after(:each) do
+    @server.stop
   end
 end
